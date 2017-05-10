df8642c7c395582c1a12d1d5b825e7d46c008056
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2017-05-10 15:36:31 +0100

    Fix sha256
    Generated using:shasum -a 256 tranor_0.1.0_darwin_amd64.tar.gz

--- Formula/tranor.rb
+++ Formula/tranor.rb
@@ -3,7 +3,7 @@ class Tranor < Formula
   homepage "https://github.com/ef-ctx/tranor"
   url "https://github.com/ef-ctx/tranor/releases/download/0.1.0/tranor_0.1.0_darwin_amd64.tar.gz"
   version "0.1.0"
-  sha256 "84031da6d867692fed9b48fa57dd4150520332b2834177444a359de63058c501"
+  sha256 "adac0e0b955b352e4183d44163239cc15f06558e55d8d09e636fdd45ac3a223e"
   def install
     bin.install "tranor"
   end
